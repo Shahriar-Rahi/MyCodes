91ea8787e075bc2cae79233b2303eec653cb5ff3
Shahriar-Rahi <md.alshahriarrahi@gmail.com>
2017-08-24 02:39:31 +0600

    Initial Stage

---
+++ LCS.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+int lcs(string X, string Y, int m, int n )
+{
+   int i, j, l;
+   int L[m+1][n+1];
+   
+   for (i=0; i<=m; i++)
+   {
+     for (j=0; j<=n; j++)
+     {
+       if (i == 0 || j == 0)
+         L[i][j] = 0;
+  
+       else if (X[i-1] == Y[j-1])
+         L[i][j] = L[i-1][j-1] + 1;
+  
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]);
+     }
+   }
+   l = L[m][n];
+   i = m, j = n;
+   char lcstring[l];
+   while (i > 0 && j > 0)
+   {
+      if (X[i-1] == Y[j-1])
+      {
+          lcstring[l-1] = X[i-1];
+          i--; j--; l--;    
+      }
+      else if (L[i-1][j] > L[i][j-1])
+         i--;
+      else
+         j--;
+   }
+   cout << "LCS of " << X << " and " << Y << " is: " << lcstring << endl;
+   return L[m][n];
+}
+
+
+  
+int main()
+{
+  string X = "";
+  string Y = "";
+  cin >> X;
+  cin >> Y;
+  
+  int m = X.length();
+  int n = Y.length();
+  cout << "Length of LCS: "<< lcs(X, Y, m, n) << endl;
+  
+ 
+  return 0;
+}
---
+++ MatChainMul.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+ 
+int MatrixChainMultiplication(int p[], int n)
+{
+ 
+    int m[n][n];
+ 
+    int i, j, k, L, mi;
+ 
+    for (i=1; i<n; i++)
+        m[i][i] = 0;
+ 
+    for (L=2; L<n; L++)
+    {
+        for (i=1; i<n-L+1; i++)
+        {
+            j = i+L-1;
+            m[i][j] = INT_MAX;
+            for (k=i; k<=j-1; k++)
+            {
+                mi = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if (mi < m[i][j])
+                    m[i][j] = mi;
+            }
+        }
+    }
+ 
+    return m[1][n-1];
+}
+ 
+int main()
+{
+	int arr[] = {950, 10, 20, 15, 35, 15, 5, 10, 20, 25630, 35, 15, 5, 10, 20, 25};
+    
+    int size = sizeof(arr)/sizeof(arr[0]);
+ 
+    cout << "Minimum number of multiplications is: " << MatrixChainMultiplication(arr, size) << endl;
+ 
+    return 0;
+}
