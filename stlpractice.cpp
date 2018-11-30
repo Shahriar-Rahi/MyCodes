5517e31adbb77542e64b9c34211803c4bc49c13d
Shahriar Rahi <md.alshahriarrahi@gmail.com>
2018-11-30 18:24:49 +0600

    was practicing stl
    forgot almost everything :(

--- stlpractice.cpp
+++ stlpractice.cpp
@@ -72,4 +72,14 @@ int main(){
         cout << goru <<" ase " << m[ goru ] << " ta :D " << endl;        
     }
     
+    
+        char line[1000];
+    while( gets( line ) ) {
+        stringstream ss( line ); // initialize kortesi
+        int num; vector< int > v;
+        while( ss >> num ) v.push_back( num ); // :P
+        sort( v.begin(), v.end() );
+        // print routine
+    }
+    
 */
